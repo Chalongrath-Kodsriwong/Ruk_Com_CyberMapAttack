550fef59631c0bc331b5c3863d7913a2a46e0330
Chalongrath-Kodsriwong <bonuszaa125@gmail.com>
2025-02-19 11:58:35 +0700

    save2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./css/Navbar.css";
-import logo from "../../public/image.png";
+import logo from "/public/image.png";
 import Count_Attack from "./Count_Attack";
 import axios from "axios";
 
